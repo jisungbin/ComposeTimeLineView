df95f8e310c3948c448e3d476d510437c6688933
jisungbin <ji@sungb.in>
2021-07-27 21:27:56 +0900

    Update gradle version

--- timelineview/src/main/java/me/sungbin/timelineview/TimeLine.kt
+++ timelineview/src/main/java/me/sungbin/timelineview/TimeLine.kt
@@ -10,7 +10,6 @@ import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 
